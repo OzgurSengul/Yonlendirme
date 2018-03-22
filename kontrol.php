717a3feecb4fdc0122dafb221f439681ec41fa12
OzgurSengul <ozgur.sengul35@hotmail.com>
2018-03-22 11:17:14 +0300

    Update kontrol.php

--- kontrol.php
+++ kontrol.php
@@ -2,8 +2,6 @@
 if(isset($_POST["eposta"])&&isset($_POST["sifre"])){
 $eposta=trim(strip_tags($_POST["eposta"]));
 $sifre=$_POST["sifre"];
-echo $eposta."<br>";
-echo $sifre."<br>";
 if(strstr($eposta, "@hotmail.com"))
 echo header("Location: http://www.deu.edu.tr/");
 if(strstr($eposta, "@gmail.com"))
